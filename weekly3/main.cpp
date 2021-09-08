546b6d3471a4644b32f4332b95f25e331cc209eb
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2021-09-08 09:45:42 +0200

    Added doc-string to randNumb() and various other comments.

--- weekly3/main.cpp
+++ weekly3/main.cpp
@@ -13,20 +13,21 @@ int main() {
 	int numGuesses = 0;
 	
 	while (randyn != 'n' && randyn != 'y') {
+		// asks whether to generate random number:
 		std::cout << "Generate a random number? y/n: ";
 		std::cin >> randyn;
 		randyn = tolower(randyn);
 
 		switch (randyn)
 		{
-			case 'n':
+			case 'n':  // asks for number to guess:
 				std::cout << "Enter a number to guess: ";
 				std::cin >> number;
 				std::cout << std::endl;
 				break;
-			case 'y':
+			case 'y':  // generates random number:
 				number = randNumb();
-			default:
+			default:  // asks for correct input:
 				std::cout << "Please answer 'y' or 'n'." << std::endl;
 				break;
 		}
@@ -35,12 +36,14 @@ int main() {
 
 	while (guess != number)
 	{
+		// asks for guess:
 		std::cout << "Guess a number: ";
 		std::cin >> guess;
 		std::cout << std::endl;
 		system("cls");
 		numGuesses++;
 
+		// checks guess:
 		if (guess < number)
 		{
 			std::cout << guess << " is too low! Guess again." << std::endl;
@@ -59,11 +62,18 @@ int main() {
 }
 
 int randNumb() {
+	// Function generating random integer from uniform distibutrion
+	// in given interval using 64bit Mersenne-Twister (the superior
+	// random number generator).
+
 	std::random_device rd;
 	std::mt19937_64 gen(rd());
 	std::uniform_int_distribution<int> RNG(0, 5);
+
+	// gives interval for random number to user:
 	std::cout << "Target number randomly chosen between 0 and 5." << std::endl;
 	Sleep(1500);
+	system("cls");
 
 	return RNG(gen);
 }
